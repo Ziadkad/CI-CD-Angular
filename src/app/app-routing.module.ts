28646c1fcb911a70a282cb4714ca6cb964a51d9d
KADDARI Ziad <ziadkaddari1@gmail.com>
2024-08-20 00:10:52 +0100

    First

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {AddProductComponent} from "./components/add-product/add-product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent},
+  { path:'add',component :AddProductComponent },
+  { path:'update/:id',component :AddProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
